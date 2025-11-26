b7335d742b2402ee19759b63319c22258c270b69
MadhanBandi25 <madhannn25@gmail.com>
2025-11-26 16:34:28 +0530

    adding the changes

--- src/main/java/com/spring/application/service/AccountServiceImpl.java
+++ src/main/java/com/spring/application/service/AccountServiceImpl.java
@@ -2,7 +2,7 @@ package com.spring.application.service;
 
 import com.spring.application.dto.AccountDto;
 import com.spring.application.entity.Account;
-import com.spring.application.mapper.AccountMapper;
+import com.spring.application.mapper.AccountMapper;d
 import com.spring.application.repository.AccountRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -111,6 +111,8 @@ public class AccountServiceImpl implements AccountService {
         return null;
     }
 
+
+    // madhan babu
 
 
 }
